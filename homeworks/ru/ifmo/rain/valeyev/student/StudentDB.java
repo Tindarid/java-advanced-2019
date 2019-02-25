558d0690c785a93c72ce69e3b954a214de862ee1
Nursan Valeyev <tindarid@gmail.com>
2019-02-25 15:17:48 +0300

    fix hw3

--- homeworks/ru/ifmo/rain/valeyev/student/StudentDB.java
+++ homeworks/ru/ifmo/rain/valeyev/student/StudentDB.java
@@ -11,7 +11,7 @@ import java.util.stream.*;
 public class StudentDB implements StudentGroupQuery {
     private static final Comparator<Student> nameComparator = Comparator
         .comparing(Student::getLastName, String::compareTo)
-        .thenComparing(Student::getLastName, String::compareTo)
+        .thenComparing(Student::getFirstName, String::compareTo)
         .thenComparing(Student::compareTo);
 
     private <T extends Collection<String>> T transform(List<Student> students, Function<Student, String> functor, Collector<String, ?, T> collector) {
@@ -67,33 +67,28 @@ public class StudentDB implements StudentGroupQuery {
         return sort(students, nameComparator);
     }
 
-    private Stream<Student> filter(Collection<Student> students, Predicate<Student> predicate) {
-        return students.stream().filter(predicate);
-    }
-
-    private List<Student> filterAndSort(Collection<Student> students, Predicate<Student> predicate) {
-        return filter(students, predicate).sorted(nameComparator).collect(Collectors.toList());
+    private <T> T filterAndSort(Collection<Student> students, Predicate<Student> predicate, Collector<Student, ?, T> collector) {
+        return students.stream().filter(predicate).sorted(nameComparator).collect(collector);
     }
 
     /** Returns list of students having specified first name. Students are ordered by name. */
     public List<Student> findStudentsByFirstName(Collection<Student> students, String name) {
-        return filterAndSort(students, student -> student.getFirstName().equals(name));
+        return filterAndSort(students, student -> student.getFirstName().equals(name), Collectors.toList());
     }
 
     /** Returns list of students having specified last name. Students are ordered by name. */
     public List<Student> findStudentsByLastName(Collection<Student> students, String name) {
-        return filterAndSort(students, student -> student.getLastName().equals(name));
+        return filterAndSort(students, student -> student.getLastName().equals(name), Collectors.toList());
     }
 
     /** Returns list of students having specified groups. Students are ordered by name. */
     public List<Student> findStudentsByGroup(Collection<Student> students, String group) {
-        return filterAndSort(students, student -> student.getGroup().equals(group));
+        return filterAndSort(students, student -> student.getGroup().equals(group), Collectors.toList());
     }
 
     /** Returns map of group's student last names mapped to minimal first name. */
     public Map<String, String> findStudentNamesByGroup(final Collection<Student> students, final String group) {
-        return filter(students, student -> student.getGroup().equals(group))
-            .collect(Collectors.toMap(Student::getLastName, Student::getFirstName, BinaryOperator.minBy(String::compareTo)));
+        return filterAndSort(students, student -> student.getGroup().equals(group), Collectors.toMap(Student::getLastName, Student::getFirstName, BinaryOperator.minBy(String::compareTo)));
     }
 
     private Stream<Map.Entry<String, List<Student>>> grouping(Collection<Student> students) {
@@ -101,7 +96,7 @@ public class StudentDB implements StudentGroupQuery {
     }
 
     private List<Group> sortedGroups(Collection<Student> students, Function<List<Student>, List<Student>> functor) {
-        return grouping(students).map(entry -> new Group(entry.getKey(), functor.apply(entry.getValue()))).collect(Collectors.toList());
+        return grouping(students).map(entry -> new Group(entry.getKey(), functor.apply(entry.getValue()))).sorted(Comparator.comparing(Group::getName)).collect(Collectors.toList());
     }
 
     /** Returns student groups, where both groups and students within a group are ordered by name. */
