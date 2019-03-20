954d1fb78f29cc4f4bb141f166ffd58388dc3339
Nursan Valeyev <tindarid@gmail.com>
2019-03-20 15:55:42 +0300

    scripts improved

--- homeworks/scripts/hw_test.sh
+++ homeworks/scripts/hw_test.sh
@@ -5,6 +5,7 @@ classpath=../java
 usage="Usage: hw_test [number] [hard or easy]"
 my=ru.ifmo.rain.valeyev
 gosha=info.kgeorgiy.java.advanced
+exec="java -cp $classpath -p $modulepath -m"
 
 if [ $# != 2 ]
 then
@@ -12,46 +13,59 @@ then
   exit
 fi
 
-if [ $2 != easy ]
+if [[ $2 != easy && $2 != hard ]]
 then
-  if [ $2 != hard ]
-  then
-    echo $usage
-    exit
-  fi
+  echo $usage
+  exit
 fi
 
 if [ $1 == 1 ]
 then
   if [ $2 == easy ]
   then
-    java -cp $classpath -p $modulepath -m $gosha.walk Walk $my.walk.RecursiveWalk
+    $exec $gosha.walk Walk $my.walk.RecursiveWalk
   else
-    java -cp $classpath -p $modulepath -m $gosha.walk RecursiveWalk $my.walk.RecursiveWalk
+    $exec $gosha.walk RecursiveWalk $my.walk.RecursiveWalk
   fi
 elif [ $1 == 2 ]
 then
   if [ $2 == easy ]
   then
-    java -cp $classpath -p $modulepath -m $gosha.arrayset SortedSet $my.arrayset.ArraySet
+    $exec $gosha.arrayset SortedSet $my.arrayset.ArraySet
   else
-    java -cp $classpath -p $modulepath -m $gosha.arrayset NavigableSet $my.arrayset.ArraySet
+    $exec $gosha.arrayset NavigableSet $my.arrayset.ArraySet
   fi
 elif [ $1 == 3 ]
 then
   if [ $2 == easy ]
   then
-    java -cp $classpath -p $modulepath -m $gosha.student StudentQuery $my.student.StudentDB
+    $exec $gosha.student StudentQuery $my.student.StudentDB
   else
-    java -cp $classpath -p $modulepath -m $gosha.student StudentGroupQuery $my.student.StudentDB
+    $exec $gosha.student StudentGroupQuery $my.student.StudentDB
   fi
 elif [ $1 == 4 ]
 then
   if [ $2 == easy ]
   then
-    java -cp $classpath -p $modulepath -m $gosha.implementor interface $my.implementor.Implementor
+    $exec $gosha.implementor interface $my.implementor.Implementor
   else
-    java -cp $classpath -p $modulepath -m $gosha.implementor class $my.implementor.Implementor
+    $exec $gosha.implementor class $my.implementor.Implementor
+  fi
+elif [[ $1 == 5 || $1 == 6 ]]
+then
+  if [ $2 == easy ]
+  then
+    $exec $gosha.implementor jar-interface $my.implementor.JarImplementor
+  else
+    $exec $gosha.implementor jar-class $my.implementor.JarImplementor
+  fi
+elif [ $1 == 7 ]
+then
+  if [ $2 == easy ]
+  then
+    $exec $gosha.concurrent scalar $my.concurrent.IP
+  else
+    $exec $gosha.concurrent list $my.concurrent.IP
   fi
 else
   echo $usage
